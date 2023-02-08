705f2412cdce1acd62edbc4e0326e10e0cdb8e7c
Sergei Utochkin <dzaga73i98@gmail.com>
2023-02-08 20:05:13 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,8 @@
 public class PhoneBook {
+    private String name;
+    private Integer phoneNumber;
+
+    public int add (String name, Integer phoneNumber){
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,35 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeMap;
+
 public class PhoneBookTest {
+    private static Random random = new Random();
+
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        int result = 0;
+
+        byte[] array = new byte[7];
+        random.nextBytes(array);
+        String generatedString = new String(array, StandardCharsets.UTF_8);
+        int generatedInt = random.nextInt();
+
+        TreeMap<String, Integer> map = new TreeMap<>();
+
+        int countContacts = phoneBook.add(generatedString, generatedInt);
+
+        if (!map.containsKey(generatedString)) {
+            map.put(generatedString,generatedInt);
+            Set<String> strings = map.keySet();
+            result = strings.size();
+        } else {
+            System.out.println("Такое имя уже есть, не можем добавить");
+        }
+        Assertions.assertEquals(countContacts, result);
+    }
 }
